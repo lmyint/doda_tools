bbb91e6e39f447a51cb465fe3532efd211d8ed9d
Leslie Myint <leslie.myint@gmail.com>
2017-02-03 07:44:51 -0500

    paste num -> string

--- makeDatasetIDs.R
+++ makeDatasetIDs.R
@@ -1,7 +1,7 @@
 set.seed(1)
 
 ids <- sapply(seq_len(2500), function(i) {
-	sample(0:9, size = 10, replace = TRUE)
+	paste0(sample(0:9, size = 10, replace = TRUE), collapse = "")
 })
 ids <- unique(ids)
 write(ids, "ids_datasets.txt")
